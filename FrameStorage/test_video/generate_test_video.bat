ad68338e63d23a87e401edd1d4589c304b951b32
alexander-113116 <alexander.113116@gmail.com>
2020-10-11 22:13:04 +0300

    Extended batch

--- FrameStorage/test_video/generate_test_video.bat
+++ FrameStorage/test_video/generate_test_video.bat
@@ -7,7 +7,14 @@ ffmpeg -y -loop 1 -i img_white.png -c:v libx264 -t 15 -pix_fmt yuv420p -s 320x24
 
 
 @REM ffmpeg -y -loop 1 -i img_white.png -c:v libx265 -t 15 -pix_fmt yuv420p -s 320x240 -vf "drawtext=text='%%{n}'" tst_265.mp4
-ffmpeg -y -loop 1 -i img_white.png -r 100 -c:v libx264 -t 5 -pix_fmt yuv420p -s 320x240 -vf "drawtext=text='%%{n}'" tst_r100_264.mp4
+ffmpeg -y -loop 1 -i img_white.png -c:v libx264 -t 5 -pix_fmt yuv420p -s 320x240 -vf "drawtext=text='%%{n}'" tst_r24_264.mp4
+
+@REM ffmpeg -y -loop 1 -i img_white.png -r 10000 -c:v libx264 -t 15 -pix_fmt yuv420p -s 320x240 tst_r10000_264.mp4
+@REM ffmpeg -y -i tst_r10000_264.mp4 -vf "drawtext=text='%%{n}'" tst_r10000_264_txt.mp4
+
+ffmpeg -y -loop 1 -i img_white.png -r 100 -c:v libx264 -t 15 -pix_fmt yuv420p -s 320x240 tst_r100_264.mp4
+ffmpeg -y -i tst_r100_264.mp4 -vf "drawtext=text='%%{n}'" tst_r100_264_txt.mp4
+
 ffmpeg -y -loop 1 -i img_white.png -r 1 -c:v libx264 -t 15 -pix_fmt yuv420p -s 320x240 tst_r1_264.mp4
 ffmpeg -y -i tst_r1_264.mp4 -vf "drawtext=text='%%{n}'" tst_r1_264_txt.mp4
 
